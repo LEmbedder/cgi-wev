cbe4978a522b2199e601dbbc1e70c7fcef7c7a5a
Lembedder <814699480@qq.com>
2019-04-23 23:29:37 +0800

    使用post方法打印的文件

---
+++ cgi-bin/input.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+	//设置HTML语言
+	printf("Content-type:text/html\n\n");
+	printf("<title>hello</title>");
+	printf("%s<br><br>", getenv("QUERY_STRING"));
+	printf("%s<br><br>", getenv("REQUEST_METHOD"));//POST
+   	char szPost[256] = {0};
+ 
+	//获取输入
+	gets(szPost);
+ 
+        //打印输入的内容
+	printf("%s<br><br>", szPost);
+	#if 1
+	char *p = szPost + 4; 
+	char*p1 = strchr(szPost, '&');
+	*p1 = '\0';
+
+	char cmd[256] = {0};
+
+	//字符串映射
+	sprintf(cmd, "%s", p);
+
+
+	//以读的方式打开1.txt
+	FILE* fp = fopen(cmd, "r");
+ 
+    //循环成立的条件是没有读到文件结尾
+    while(!feof(fp))
+    {
+        //每次从文件中读取1个字符
+        char ch = fgetc(fp);
+ 
+        //当读取到\n时
+        if('\n' == ch)
+        {
+            //打印换行
+            printf("<br><br>");
+        }
+       else
+        {
+            //打印字符
+            putchar(ch);
+        }
+    }
+close(fp);
+
+#endif
+
+}
